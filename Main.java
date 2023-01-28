edff3fa803100f9e38e05da75d22719836bb1a3f
huibvanalphen <123832943+huibvanalphen@users.noreply.github.com>
2023-01-28 19:34:53 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner myName = new Scanner(System.in);
+        System.out.println("Enter username: ");
+
+        String userName = myName.nextLine();
+        sayHello (userName);
+    }
+
+    private static void sayHello(String name) {
+        System.out.println("Hello " + name);
+        System.out.println("Daag " + name);
+        System.out.println("Hoi " + name);
+
+    }
+}
+
