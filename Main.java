39842af8a0ab88022bc211c446727e72055baa42
huibvanalphen <huib.van.alphen@gmail.com>
2023-01-29 16:03:12 +0100

    Wijziging-4 van Zondag 29 januari

--- Main.java
+++ Main.java
@@ -4,21 +4,22 @@ class Main {
     public static void main(String[] args) {
 
         Scanner myName = new Scanner(System.in);
-        System.out.println("Enter username: ");
-
+        System.out.print("Enter username: ");
         String userName = myName.nextLine();
-        sayHello (userName);
 
+        for (int i = 0; i < 5; i++) {
+            sayHello(userName);
         }
 
-        private static void sayHello(String name) {
-            System.out.println("Hello " + name);
-            System.out.println("Daag " + name);
-            System.out.println("Hoi " + name);
-            System.out.println("Zondag-1 29-jan " + name);
-            System.out.println("Zondag-2 29-jan " + name);
-            System.out.println("Zondag-3 29-jan " + name);
+        int i = 0;
+        while (i < 5) {
+            sayHello(userName);
+            i++;
+        }
+    }
 
+    private static void sayHello (String name) {
+        System.out.println("Hello " + name);
     }
 
 }
